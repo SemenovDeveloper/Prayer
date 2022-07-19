462b2e0efa26d1f225203237b6e00571aeba0104
Alexander Semenov <semenovdev@yahoo.com>
2022-07-19 08:56:11 +0600

    init project

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello, world!</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  
+})
+
+export default App;
